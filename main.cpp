2fb815786c26878888448d0b92974615cc9ae9dc
FShaaM <zinovevas135@gmail.com>
2026-04-07 18:46:34 +0300

    Finished Homework

--- main.cpp
+++ main.cpp
@@ -6,61 +6,90 @@ using zinoviev::Vector;
 
 bool test_getSie(const char** teastName)
 {
-	*teastName = __func__;
-	Vector< int > v(2ull, 5);
-	return !v.getSize();
+    *teastName = __func__;
+    Vector< int > v(2ull, 5);
+    return !v.getSize();
 }
 
 bool test_Non_mempty(const char** teastName)
 {
-	*teastName = __func__;
-	Vector< int > v(2ull, 5);
+    *teastName = __func__;
+    Vector< int > v(2ull, 5);
 
-	return v.getSize() == 5;
+    return v.getSize() == 5;
 }
 
 bool test_Constract_and_Destract(const char** teastName)
 {
-	*teastName = __func__;
-	Vector< int > v(2ull, 5);
-	return true;
+    *teastName = __func__;
+    Vector< int > v(2ull, 5);
+    return true;
 }
 
 bool test_Default_Vec_is_empty(const char** teastName)
 {
-	*teastName = __func__;
-	Vector< int > v(2ull, 5);
-	return v.is_empty();
+    *teastName = __func__;
+    Vector< int > v(2ull, 5);
+    return v.is_empty();
+}
+
+bool test_push_back(const char** teastName)
+{
+    *teastName = __func__;
+    Vector< int > v(2ull, 5);
+    try
+    {
+        v.push_back(6);
+        return true;
+    }
+    catch (...)
+    {
+        return false;
+    }
+}
+
+bool test_pop_back(const char** teastName)
+{
+    *teastName = __func__;
+    Vector< int > v(5, 5);
+    v.pop_back();
+
+    return v.getSize() == 4;
 }
 
 
 int main()
 {
-	using test_t = bool(*)(const char**);
-	using case_t = std::pair<test_t, const char*>;
+    using test_t = bool(*)(const char**);
+    using case_t = std::pair<test_t, const char*>;
 
-	case_t tests[] = {
-		{test_Constract_and_Destract, "Vect must be default"},
-	    {test_Default_Vec_is_empty, "Defult Vector must be empty"},
-		{test_getSie, "Size of empty vector must be 0"},
-		{test_Non_mempty, "Size of non-empty vector must be !0"}
-	};
+    case_t tests[] = {
+      {test_Constract_and_Destract, "Vect must be default"},
+      {test_Default_Vec_is_empty, "Defult Vector must be empty"},
+      {test_getSie, "Size of empty vector must be 0"},
+      {test_Non_mempty, "Size of non-empty vector must be !0"},
+      {test_push_back, "Bad push_back"},
+      {test_pop_back, "Bad pop_back"}
+    };
 
-	size_t f = 0;
-	constexpr size_t count = sizeof(tests) / sizeof(test_t);
-	for (size_t i = 0; i < count; ++i)
-	{
-		const char* teastName = nullptr;
-		bool r = tests[i].first(&teastName);
+    size_t f = 0;
+    constexpr size_t count = sizeof(tests) / sizeof(tests[0]);
+    for (size_t i = 0; i < count; ++i)
+    {
+        const char* teastName = nullptr;
+        bool r = tests[i].first(&teastName);
 
-		if (!r)
-		{
-			++f;
-			std::cout << "Error:" << teastName << "\n";
-			std::cout << "\t" << tests[i].second << "\n";
-		}
-	}
+        if (!r)
+        {
+            ++f;
+            std::cout << "Error: " << teastName << "\n";
+            std::cout << "\t" << tests[i].second << "\n";
+        }
+    }
 
-	std::cout << "Summer:\n\t" << (count - f) << "\n";
-	std::cout << "Fail" << count << "total\n";
+    std::cout << "\n";
+
+    std::cout << "Pass:\t" << (count - f) << "\n";
+    std::cout << "Fail\t" << f << "\n";
+    std::cout << "Summer:\t" << count << "\n";
 }
--- pe_vector.hpp
+++ pe_vector.hpp
@@ -5,69 +5,116 @@
 
 namespace zinoviev
 {
-	template <class T>
-	struct Vector
-	{
-		Vector();
-		~Vector();
-		Vector(size_t size, const T& val);
-		explicit Vector(size_t size);
+    template <class T>
+    struct Vector
+    {
+        Vector();
+        ~Vector();
+        Vector(size_t size, const T& val);
+        explicit Vector(size_t size);
 
-		bool is_empty() const noexcept;
-		size_t getSize() const noexcept;
+        bool is_empty() const noexcept;
+        size_t getSize() const noexcept;
 
-		//void push_back(const T&);              // H.W. и тесты
-		//void pop_back();                       // H.W. и тесты
+        void push_back(const T& x);              // H.W. и тесты
+        void pop_back();                       // H.W. и тесты
 
-		Vector<T>& operator=(const Vector<T>&) = delete;
+        Vector<T>& operator=(const Vector<T>&) = delete;
 
-	private:
-		T* data_;
-		size_t size_;
-		size_t capasity_;
-	};
+    private:
+        T* data_;
+        size_t size_;
+        size_t capasity_;
+    };
 }
 
 template <class T>
 zinoviev::Vector<T>::~Vector()
 {
-	delete[] data_;
+    delete[] data_;
 }
 
 template <class T>
 zinoviev::Vector<T>::Vector() :
-	data_(nullptr),
-	size_(0),
-	capasity_(0)
-{}
+    data_(nullptr),
+    size_(0),
+    capasity_(0)
+{
+}
 
 template <class T>
 zinoviev::Vector<T>::Vector(size_t size) :
-	data_(size ? new T[size] : nullptr),
-	size_(size),
-	capasity_(size)
-{}
+    data_(size ? new T[size] : nullptr),
+    size_(size),
+    capasity_(size)
+{
+}
 
 template <class T>
 zinoviev::Vector<T>::Vector(size_t size, const T& val) :
-	Vector(size)                                                      // для делигирования
+    Vector(size)                                                      // для делигирования
 {
-	for (size_t i = 0; i < size; ++i)
-	{
-		data_[i] = val;                                          // теперь вызывается конструктор и деструктор (можно без try/catch)
-	}
+  for (size_t i = 0; i < size; ++i)
+  {
+    data_[i] = val;                                          // теперь вызывается конструктор и деструктор(можно без try / catch)
+  }
 }
 
 template <class T>
 bool zinoviev::Vector<T>::is_empty() const noexcept
 {
-	return !size_;
+    return !size_;
 }
 
 template <class T>
 size_t zinoviev::Vector<T>::getSize() const noexcept
 {
-	return size_;
+    return size_;
+}
+
+template <class T>
+void zinoviev::Vector<T>::push_back(const T& x)
+{
+    if (size_ == capasity_)
+    {
+        size_t new_cap = (capasity_ == 0 ? 2 : capasity_ * 2);
+        T* new_data = nullptr;
+        try
+        {
+            new_data = new T[new_cap];
+
+            for (size_t i = 0; i < size_; ++i)
+            {
+                new_data[i] = data_[i];
+            }
+            new_data[size_] = x;
+            ++size_;
+
+            delete[] data_;
+            data_ = new_data;
+            capasity_ = new_cap;
+        }
+        catch (...)
+        {
+            delete[] new_data;
+            throw;
+        }
+    }
+    else
+    {
+        data_[size_] = x;
+        ++size_;
+    }
+}
+
+template <class T>
+void zinoviev::Vector<T>::pop_back()
+{
+    if (!data_ || !size_)
+        return;
+
+    --size_;
+    data_[size_].~T();
 }
 
 #endif
